9d062f4d8ef03be65797376d5f3ce9919b9745b5
alisidaniel <alisidaniel@gmail.com>
2022-01-09 22:20:08 +0200

    added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ProductModule } from './product/product.module';
@@ -8,6 +9,7 @@ import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
